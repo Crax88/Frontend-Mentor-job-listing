563477350080cf58a6a32864f418a9840778e514
Crax <ntrafilkin88@gmail.com>
2022-08-03 15:43:03 +0300

    Job card

---
+++ src/components/JobCard.tsx
@@ -0,0 +1,45 @@
+import { Card } from "./Card";
+import { Badge } from "./Badge";
+import { TFilter, TJob } from "../App";
+
+type TProps = {
+  job: TJob;
+  onSelectFilter: (filter: TFilter) => void;
+};
+
+export const JobCard = ({ job, onSelectFilter }: TProps) => {
+  const jobTags = [job.role, job.level, ...job.languages, ...job.tools];
+  return (
+    <Card isFeatured={job.featured}>
+      <img className="job-img" src={job.logo} alt={job.company} />
+      <div className="job-info">
+        <div className="job-info_main">
+          <h3>{job.company}</h3>
+          {job.new && (
+            <Badge variant="rounded" colorTheme="primary">
+              NEW!
+            </Badge>
+          )}
+          {job.featured && (
+            <Badge variant="rounded" colorTheme="dark">
+              FEATURED
+            </Badge>
+          )}
+        </div>
+        <div className="job-info_position">{job.position}</div>
+        <div className="job-info_meta">
+          <span>{job.postedAt}</span>
+          <span>{job.contract}</span>
+          <span>{job.location}</span>
+        </div>
+      </div>
+      <div className="job-tags">
+        {jobTags.map((tag) => (
+          <Badge key={tag} onClick={() => onSelectFilter(tag)}>
+            {tag}
+          </Badge>
+        ))}
+      </div>
+    </Card>
+  );
+};
