70ef903be4323ce8695ccb857900cc6787fa9abb
Crax <ntrafilkin88@gmail.com>
2022-08-03 15:20:22 +0300

    Filter panel delete and clear

--- src/components/FilterPanel.tsx
+++ src/components/FilterPanel.tsx
@@ -1,22 +1,32 @@
 import { Card } from "./Card";
 import { Badge } from "./Badge";
+import { TFilter } from "../App";
 
 type TProps = {
   filters: string[];
+  onRemoveFilter: (filter: TFilter) => void;
+  onClearFilter: () => void;
 };
 
-export const FilterPanel = ({ filters }: TProps) => {
+export const FilterPanel = ({
+  filters,
+  onRemoveFilter,
+  onClearFilter,
+}: TProps) => {
+  if (filters.length === 0) {
+    return null;
+  }
   return (
     <Card className="filter-panel">
       <div className="filter-panel_list">
         {filters.map((filter) => (
-          <Badge key={filter} variant="clearable">
+          <Badge key={filter} variant="clearable" onClear={onRemoveFilter}>
             {filter}
           </Badge>
         ))}
       </div>
 
-      <button>Clear</button>
+      <button onClick={onClearFilter}>Clear</button>
     </Card>
   );
 };
